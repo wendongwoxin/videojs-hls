c7570ed9bbb8815a50150800576d9cf609686f08
wujinwen <1844287682@qq.com>
2020-11-23 15:30:57 +0800

    代码提交

---
+++ src/components/video/VideoPlay.vue
@@ -0,0 +1,31 @@
+<template>
+    <video id="my-video" class="video-js vjs-default-skin" controls preload="auto" :style="{width:this.width,height:this.height}">
+        <source :src="hlsSource" type="application/x-mpegURL">
+    </video>
+</template>
+<script>
+import videojs from 'video.js'
+import 'videojs-contrib-hls'
+export default {
+    name: 'VideoPlay',
+    props: {
+        options: Object,
+        hlsSource: String,
+        width: String,
+        height: String
+    },
+    data () {
+        return {
+            player: null
+        }
+    },
+    mounted () {
+        this.player =videojs('my-video', this.options)
+    },
+    methods: {
+        play () {
+            this.player.play()
+        }
+    }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <VideoPlay class="video" :options="playOptions" :hlsSource="hlsSource" ref="videoplay" width="1080px" height="720px"/>
+    <el-button class='btn' type="primary" @click="play">播放</el-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import VideoPlay from '@/components/video/VideoPlay'
+
+export default {
+  name: 'Home',
+  components: {
+    VideoPlay
+  },
+  data () {
+    return {
+      playOptions: {
+        bigPlayButton: false,
+        textTrackDisplay: false,
+        posterImage: true,
+        errorDisplay: false,
+        controlBar: true
+      },
+      hlsSource: 'http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8'
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      // this.$refs.videoplay.play() 
+      // play() failed because the user didn't interact with the document first  【用户没有先去跟网页做交互再执行音频播放】
+    })
+  },
+  methods: {
+    play () {
+      this.$refs.videoplay.play()
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  text-align: center;
+  .video {
+    margin: 10px auto;
+  }
+}
+</style>
